2ad56888efab1827d6d6b25bdd2eab0718af1f08
JericoL <Jericolabajo11@gmail.com>
2024-11-29 17:58:00 +0800

    Add project files.

---
+++ NDV_PetLoversClinic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NDV_PetLoversClinic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
